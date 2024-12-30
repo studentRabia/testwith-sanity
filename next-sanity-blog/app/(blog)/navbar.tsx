89c90775bb6a1614fc236f703f21fd7a6eaefea9
studentRabia <pubgali125.com@gmail.com>
2024-12-31 01:59:47 +0500

    first commit

---
+++ next-sanity-blog/app/(blog)/navbar.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-gray-800 text-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex items-center">
+            <h1 className="text-2xl font-bold">Trend</h1>
+          </div>
+          <div className="hidden md:flex space-x-4">
+            <Link href="/" className="hover:text-gray-300">
+              Home
+            </Link>
+
+            <Link href="/blog" className="hover:text-gray-300">
+              Blog
+            </Link>
+
+            <Link href="/contact" className="hover:text-gray-300">
+              Contact{" "}
+            </Link>
+          </div>
+          <div className="md:hidden">
+            <button
+              title="click btn"
+              className="text-gray-400 hover:text-white focus:outline-none"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              href="/"
+              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
+            >
+              Home
+            </Link>
+
+            <Link
+              href="/"
+              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
+            >
+              Blog
+            </Link>
+
+            <Link
+              href="/"
+              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+}
